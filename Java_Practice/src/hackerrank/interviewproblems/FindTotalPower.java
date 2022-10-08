2974370a22cd70a04bf13e9f390501248329a762
Amit Das <7amitdas@gmail.com>
2022-10-08 12:31:30 +0600

    Interview Problems from Amazon Solved.

---
+++ Java_Practice/src/hackerrank/interviewproblems/FindTotalPower.java
@@ -0,0 +1,55 @@
+package hackerrank.interviewproblems;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FindTotalPower {
+
+	/*
+	Amazon : Software Development Engineer
+	Problem Summary : Find the total power.
+	Example: [2,3,2,1]
+	
+	Matrix[0, 0] : MIN[2] * SUM[2] = 2 * 2 = 4
+	Matrix[0, 1] : MIN[2, 3] * SUM[2, 3] = 2 * 5 = 10
+	Matrix[0, 2] : MIN[2, 3, 2] * SUM[2, 3, 2] = 2 * 7 = 14
+	Matrix[0, 3] : MIN[2, 3, 2, 1] * SUM[2, 3, 2, 1] = 1 * 8 = 8
+	Matrix[1, 1] : MIN[3] * SUM[3] = 3 * 3 = 9
+	Matrix[1, 2] : MIN[3, 2] * SUM[3, 2] = 2 * 5 = 10
+	Matrix[1, 3] : MIN[3, 2, 1] * SUM[3, 2, 1] = 1 * 6 = 6
+	Matrix[2, 2] : MIN[2] * SUM[2] = 2 * 2 = 4
+	Matrix[2, 3] : MIN[2, 1] * SUM[2, 1] = 1 * 3 = 3
+	Matrix[3, 3] : MIN[1] * SUM[1] = 1 * 1 = 1
+	Total : 69
+	*/
+
+	public static int findTotalPower(List<Integer> power) {
+		// Write your code here
+		int sumTotalPower = 0;
+		List<Integer> subList;
+		int min = 0;
+		int sum = 0;
+		for ( int i = 0; i < power.size(); i++ ) {
+			for ( int j = i; j <= power.size(); j++ ) {
+				subList = power.subList(i, j);
+				if (!subList.isEmpty()) {
+					min = Collections.min(subList);
+					sum = subList.stream().collect(Collectors.summingInt(Integer::intValue));
+					sumTotalPower += (min * sum);
+				}
+			}
+		}
+		return sumTotalPower;
+	}
+
+	public static void main(String[] args) {
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		a.add(2);
+		a.add(3);
+		a.add(2);
+		a.add(1);
+		System.out.println(findTotalPower(a));
+	}
+}
---
+++ Java_Practice/src/hackerrank/interviewproblems/GetMinMoves.java
@@ -0,0 +1,53 @@
+package hackerrank.interviewproblems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetMinMoves {
+
+	/*
+	Amazon : Software Development Engineer
+	Problem Summary : Count the minimum moves required to move the low weighted plate at the top 
+	and high weighted plate at the bottom of a stack only. All other plates might be unsorted.
+	
+	All the plates has distinct weight.
+	*/
+
+	public static int getMinMoves(List<Integer> plates) {
+		// Write your code here
+		int lowWeightPlate = plates.get(0);
+		int highWeightPlate = 0;
+		for ( Integer plate : plates ) {
+			if (plate < lowWeightPlate) {
+				lowWeightPlate = plate;
+			}
+			if (plate > highWeightPlate) {
+				highWeightPlate = plate;
+			}
+		}
+		int lowIdx = plates.indexOf(lowWeightPlate);
+		int highIdx = plates.indexOf(highWeightPlate);
+
+		if (lowIdx != 0 && (highIdx != plates.size() - 1)) {
+			highIdx = highIdx + 1;
+		}
+		highIdx = plates.size() - 1 - highIdx;
+		return lowIdx + highIdx;
+	}
+
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		a.add(5);
+		a.add(2);
+		a.add(4);
+		a.add(3);
+		a.add(1);
+		a.add(6);
+		System.out.println(getMinMoves(a));
+		long end = System.currentTimeMillis();
+		float sec = (end - start) / 1000F;
+		System.out.println(sec + " seconds");
+	}
+
+}
