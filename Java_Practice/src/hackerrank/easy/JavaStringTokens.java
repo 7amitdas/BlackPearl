a16d33ea3315345d59981b6d7191121bcf23d2e7
Amit Das <7amitdas@gmail.com>
2023-05-13 14:45:08 +0800

    new problems are solved

---
+++ Java_Practice/src/hackerrank/easy/JavaStringTokens.java
@@ -0,0 +1,26 @@
+package hackerrank.easy;
+
+/**
+ * Hacker Rank Java > Strings
+ * 
+ * Java String Tokens
+ * {@link} https://www.hackerrank.com/challenges/java-string-tokens
+ */
+public class JavaStringTokens {
+
+	public static void printStringTokens(String s) {
+		if (s.trim().length() == 0) {
+			System.out.println(0);
+		} else {
+			String s1[] = s.trim().split("[ !,?._'@]+");
+			System.out.println(s1.length);
+			for (int i = 0; i < s1.length; i++) {
+				System.out.println(s1[i]);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		printStringTokens("");
+	}
+}
---
+++ Java_Practice/src/hackerrank/easy/PatternSyntaxChecker.java
@@ -0,0 +1,32 @@
+package hackerrank.easy;
+
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+/**
+ * Hacker Rank Java > Strings
+ * 
+ * Pattern Syntax Checker
+ * {@link} https://www.hackerrank.com/challenges/pattern-syntax-checker
+ */
+public class PatternSyntaxChecker {
+
+	public static void checkPattern(String s) {
+
+	}
+
+	public static void main(String[] args) {
+		int testCases = 3;
+		while (testCases > 0) {
+			String pattern = "batcatpat(nat";
+			// Write your code
+			try {
+				Pattern.compile(pattern);
+				System.out.println("Valid");
+			} catch (PatternSyntaxException e) {
+				System.out.println("Invalid");
+			}
+			testCases--;
+		}
+	}
+}
