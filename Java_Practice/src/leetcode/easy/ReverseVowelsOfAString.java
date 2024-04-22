5ad2b2a4249ae82602bb736d1950ce652bb00dc9
Amit Das <7amitdas@gmail.com>
2024-04-22 21:50:02 +0800

    Reverse of Vowels Leetcode problem

---
+++ Java_Practice/src/leetcode/easy/ReverseVowelsOfAString.java
@@ -0,0 +1,55 @@
+package leetcode.easy;
+
+/**
+ * Coding Interview Questions </br>
+ * Reverse Vowels Of A String Solution 1 </br>
+ * {@link} https://leetcode.com/problems/reverse-vowels-of-a-string
+ * 
+ * @author Amit Das
+ */
+public class ReverseVowelsOfAString {
+
+	public String reverseVowels(String s) {
+		char[] str = s.toCharArray();
+		int firstIndex = 0;
+		int lastIndex = str.length - 1;
+		boolean firstVFound = false;
+		boolean lastVFound = false;
+		while (firstIndex <= lastIndex) {
+			if (!firstVFound && isVowel(str[firstIndex])) {
+				firstVFound = true;
+			}
+			if (!lastVFound && isVowel(str[lastIndex])) {
+				lastVFound = true;
+			}
+			if (firstVFound && lastVFound) {
+				char temp = str[lastIndex];
+				str[lastIndex] = str[firstIndex];
+				str[firstIndex] = temp;
+				firstVFound = false;
+				lastVFound = false;
+				firstIndex++;
+				lastIndex--;
+			} else if (firstVFound && !lastVFound) {
+				lastIndex--;
+			} else if (!firstVFound && lastVFound) {
+				firstIndex++;
+			} else {
+				firstIndex++;
+				lastIndex--;
+			}
+		}
+		return new String(str);
+	}
+
+	public boolean isVowel(char c) {
+		return c == 'A' || c == 'a' || c == 'E' || c == 'e' || c == 'I' || c == 'i' || c == 'O' || c == 'o' || c == 'U'
+				|| c == 'u';
+	}
+
+	public static void main(String[] args) {
+		ReverseVowelsOfAString p = new ReverseVowelsOfAString();
+		System.out.println(p.reverseVowels("hello"));
+		System.out.println(p.reverseVowels("leetcode"));
+	}
+}
