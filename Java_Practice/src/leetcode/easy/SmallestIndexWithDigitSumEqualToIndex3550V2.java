06c8e2e42c7d58470efac9ad7e771303aa577bbd
Amit Das <7amitdas@gmail.com>
2025-05-19 01:01:46 +0800

    Leet code problem solved

--- Java_Practice/src/leetcode/easy/SmallestIndexWithDigitSumEqualToIndex3550V2.java
+++ Java_Practice/src/leetcode/easy/SmallestIndexWithDigitSumEqualToIndex3550V2.java
@@ -2,25 +2,33 @@ package leetcode.easy;
 
 /**
  * Coding Interview Questions </br>
- * 3550. Smallest Index With Digit Sum Equal to Index Solution 1 </br>
+ * 3550. Smallest Index With Digit Sum Equal to Index Solution 2 </br>
  * {@link} https://leetcode.com/problems/smallest-index-with-digit-sum-equal-to-index/
  * 
  * @author Amit Das
  */
-public class SmallestIndexWithDigitSumEqualToIndex3550 {
+public class SmallestIndexWithDigitSumEqualToIndex3550V2 {
 
 	public int smallestIndex(int[] nums) {
 		for (int i = 0; i < nums.length; i++) {
-			int sum = String.valueOf(nums[i]).chars().map(Character::getNumericValue).sum();
-			if (sum == i) {
+			if (getSum(nums[i]) == i) {
 				return i;
 			}
 		}
 		return -1;
 	}
 
+	private int getSum(int number) {
+		int sum = 0;
+		while (number > 0) {
+			sum = number % 10;
+			number = number / 10;
+		}
+		return sum;
+	}
+
 	public static void main(String[] args) {
-		SmallestIndexWithDigitSumEqualToIndex3550 p = new SmallestIndexWithDigitSumEqualToIndex3550();
+		SmallestIndexWithDigitSumEqualToIndex3550V2 p = new SmallestIndexWithDigitSumEqualToIndex3550V2();
 		System.out.println(p.smallestIndex(new int[] { 1, 3, 2 })); // 2
 		System.out.println(p.smallestIndex(new int[] { 1, 10, 11 })); // 1
 		System.out.println(p.smallestIndex(new int[] { 1, 2, 3 })); // -1
